e2934bad48395c122733e209cdc58548317787ef
Cole Murphy <cole.murphy@venuebook.com>
2017-11-30 18:23:54 -0500

    Begin integrating react and redux

---
+++ gulpfile.babel.js
@@ -0,0 +1,70 @@
+import gulp from 'gulp';
+import loadPlugins from 'gulp-load-plugins';
+import webpack from 'webpack';
+import rimraf from 'rimraf';
+
+const plugins = loadPlugins();
+
+import popupWebpackConfig from './popup/webpack.config';
+import backgroundWebpackConfig from './background/webpack.config';
+import injectedWebpackConfig from './injected/webpack.config';
+
+gulp.task('popup-js', ['clean'], (cb) => {
+  webpack(popupWebpackConfig, (err, stats) => {
+    if(err) throw new plugins.util.PluginError('webpack', err);
+
+    plugins.util.log('[webpack]', stats.toString());
+
+    cb();
+  });
+});
+
+gulp.task('background-js', ['clean'], (cb) => {
+  webpack(backgroundWebpackConfig, (err, stats) => {
+    if(err) throw new plugins.util.PluginError('webpack', err);
+
+    plugins.util.log('[webpack]', stats.toString());
+
+    cb();
+  });
+});
+
+gulp.task('injected-js', ['clean'], (cb) => {
+  webpack(injectedWebpackConfig, (err, stats) => {
+    if(err) throw new plugins.util.PluginError('webpack', err);
+
+    plugins.util.log('[webpack]', stats.toString());
+
+    cb();
+  });
+});
+
+gulp.task('popup-html', ['clean'], () => {
+  return gulp.src('popup/src/index.html')
+    .pipe(plugins.rename('popup.html'))
+    .pipe(gulp.dest('./build'))
+});
+
+gulp.task('copy-manifest', ['clean'], () => {
+  return gulp.src('manifest.json')
+    .pipe(gulp.dest('./build'));
+});
+
+gulp.task('copy-assets', ['clean'], () => {
+  return gulp.src('assets/**/*')
+    .pipe(gulp.dest('./build/assets'));
+});
+
+gulp.task('clean', (cb) => {
+  rimraf('./build', cb);
+});
+
+gulp.task('build', ['copy-assets', 'copy-manifest', 'popup-js', 'popup-html', 'background-js', 'injected-js']);
+
+gulp.task('watch', ['default'], () => {
+  gulp.watch('popup/**/*', ['build']);
+  gulp.watch('injected/**/*', ['build']);
+  gulp.watch('background/**/*', ['build']);
+});
+
+gulp.task('default', ['build']);
---
+++ injected/src/injected.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {render} from 'react-dom';
+import {Provider} from 'react-redux';
+import {Store} from 'react-chrome-redux';
+
+import App from './components/App';
+
+const anchor = document.createElement('div');
+anchor.id = 'tags';
+
+document.body.insertBefore(anchor, document.body.childNodes[0]);
+
+const proxyStore =  new Store({
+  state: {},
+  portName: 'test'
+});
+
+render(
+  <Provider store={proxyStore}>
+    <App/>
+  </Provider>
+  , document.getElementById('tags'));
