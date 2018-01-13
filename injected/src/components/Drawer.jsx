342deff512cdd72948d6a495b06360e2004b7808
Cole Murphy <cole.murphy@venuebook.com>
2018-01-12 20:08:23 -0500

    Pull drawer actions into new component

--- injected/src/components/Drawer.jsx
+++ injected/src/components/Drawer.jsx
@@ -2,11 +2,10 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
 import CreateBookmarkModal from './CreateBookmarkModal';
+import DrawerActions from './DrawerActions';
 import ListView from '../containers/ListView';
 import Modal from './Modal';
 import SettingsView from '../containers/SettingsView';
-
-import ifTrue from '../utils/ifTrue';
 
 export default class Drawer extends Component {
   constructor(props) {
@@ -32,16 +31,8 @@ export default class Drawer extends Component {
     })
   }
 
-  handleAddBookmark () {
-    chrome.runtime.sendMessage({ ref: 'add_bookmark' });
-  }
-
   handleChange (bookmarkKey, event) {
     this.setState({ [bookmarkKey]: event.target.value });
-  }
-
-  handleClickSettings () {
-    this.setState({ view: 'settings' });
   }
 
   handleClickSubmit () {
@@ -57,48 +48,40 @@ export default class Drawer extends Component {
     });
   }
 
-  handleClickTags () {
-    this.setState({ view: 'tags' });
-  }
+  toggleView () {
+    let { view } = this.state,
+        viewOptions = {
+          tags: 'settings',
+          settings: 'tags',
+        };
 
-  handleCloseDrawer () {
-    // send message to close the drawer
-    chrome.runtime.sendMessage({ ref: 'toggle_drawer' });
+    this.setState({ view: viewOptions[view] });
   }
 
   renderBookmarkModal () {
     let { bookmarkName, bookmarkUrl } = this.state,
         { title } = this.props.tagMarkerFolder;
 
-    Modal.render(<CreateBookmarkModal name={bookmarkName} url={bookmarkUrl} onChange={(key, e) => this.handleChange(key, e)} onSubmit={() => this.handleClickSubmit()} title={title} />);
-  }
-
-  renderActions () {
-    let { view } = this.state;
-
-    return (
-      <div className='drawer-actions'>
-        <button className='button drawer-close' onClick={() => this.handleCloseDrawer()} title='close drawer'>&raquo;</button>
-        {ifTrue(this.props.tagMarkerFolder.id).render(() => (
-          // only render add bookmark button if we have a place to put it
-          <button className='button add-bookmark' onClick={() => this.handleAddBookmark()} title='add bookmark'>+</button>
-        ))}
-        {ifTrue(this.props.tagMarkerFolder.id && view === 'settings').render(() => (
-          // only render button to leave settings if we have a folder set
-          <button className='button show-tags' onClick={() => this.handleClickTags()} title='show tags'><i className='fa fa-list'/></button>
-        ))}
-        {ifTrue(view === 'tags').render(() => (
-          <button className='button show-settings' onClick={() => this.handleClickSettings()} title='settings'><i className='fa fa-cog'/></button>
-        ))}
-      </div>
-    )
+    Modal.render(
+      <CreateBookmarkModal
+        name={bookmarkName}
+        url={bookmarkUrl}
+        onChange={(key, e) => this.handleChange(key, e)}
+        onSubmit={() => this.handleClickSubmit()}
+        title={title}
+        />
+    );
   }
 
   render () {
     return (
       <div className='drawer'>
         <div className='container'>
-          {this.renderActions()}
+          <DrawerActions
+            folderIsSet={this.props.tagMarkerFolder.id > -1}
+            toggleView={() => this.toggleView()}
+            view={this.state.view}
+            />
           {this.state.view === 'tags' ? <ListView /> : <SettingsView />}
         </div>
         <div className='modal-container' />
---
+++ injected/src/components/DrawerActions.jsx
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+import ifTrue from '../utils/ifTrue';
+
+export default class DrawerActions extends Component {
+  constructor (props) {
+    super(props);
+  }
+
+  handleAddBookmark () {
+    chrome.runtime.sendMessage({ ref: 'add_bookmark' });
+  }
+
+  handleCloseDrawer () {
+    // send message to close the drawer
+    chrome.runtime.sendMessage({ ref: 'toggle_drawer' });
+  }
+
+  render() {
+    let { folderIsSet, view } = this.props;
+    console.log('folderIsSet:', folderIsSet)
+
+    return (
+      <div className='drawer-actions'>
+        <button className='button drawer-close' onClick={() => this.handleCloseDrawer()} title='close drawer'>&raquo;</button>
+        {ifTrue(folderIsSet).render(() => (
+          // only render add bookmark button if we have a place to put it
+          <button className='button add-bookmark' onClick={() => this.handleAddBookmark()} title='add bookmark'>+</button>
+        ))}
+        {ifTrue(folderIsSet && view === 'settings').render(() => (
+          // only render button to leave settings if we have a folder set
+          <button className='button show-tags' onClick={() => this.props.toggleView()} title='show tags'><i className='fa fa-list'/></button>
+        ))}
+        {ifTrue(view === 'tags').render(() => (
+          <button className='button show-settings' onClick={() => this.props.toggleView()} title='settings'><i className='fa fa-cog'/></button>
+        ))}
+      </div>
+    )
+  }
+}
