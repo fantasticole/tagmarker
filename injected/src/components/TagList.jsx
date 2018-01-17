7d334b9a1139f31e39b4b65725c7e3d159504010
Cole Murphy <cole.murphy@venuebook.com>
2018-01-16 19:51:16 -0500

    Remove tags header

--- injected/src/components/TagList.jsx
+++ injected/src/components/TagList.jsx
@@ -12,9 +12,6 @@ export default class TagList extends Component {
   render () {
     return (
       <div className='tag-list__container'>
-        <div className='drawer__header tags__header'>
-          <h1 className='drawer__header-text'>Tags</h1>
-        </div>
         <TagActions />
         <ul className='tagmarker-list tag-list'>
           <SelectedTags />
