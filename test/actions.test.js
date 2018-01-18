9c4695101e1a886f6fd7e7dead143dc3abad14f9
Cole Murphy <cole.murphy@venuebook.com>
2018-01-18 00:16:02 -0500

    Prep action test cases

--- test/actions.test.js
+++ test/actions.test.js
@@ -1,12 +1,72 @@
 import * as actions from '../background/src/store/actions';
 
 describe('actions', () => {
-  it('should create a tag', () => {
+  // initialize
+  it('should create an action to setBookmarks with bookmarks from data param', () => {
+  });
+
+  it('should create an action to setTags with tags from data param', () => {
+  });
+
+  it('should create an action to updateFilteredBookmarks with an empty arrray', () => {
+  });
+
+  it('should create an action to updateFilteredTags with all tag ids', () => {
+  });
+
+  // createOrUpdateBookmark
+  it('should create an action to create or update a bookmark object', () => {
+    // CREATE_OR_UPDATE_BOOKMARK
+  });
+
+  it('should create an action to filterBookmarksAndTags with currently selected tags', () => {
+  });
+
+  // createOrUpdateTags
+  it('should create an action to add a tag', () => {
     const tags = [{ title: 'test' }];
     const expectedAction = {
       type: 'CREATE_OR_UPDATE_TAGS',
       tags
     }
     expect(actions.createOrUpdateTags(tags)).toEqual(expectedAction)
-  })
+  });
+
+  // filterBookmarksAndTags
+  it('should create an action to updateFilteredBookmarks with an empty array if no tags are selected', () => {
+  });
+
+  it('should create an action to updateFilteredTags with all tag ids if no tags are selected', () => {
+  });
+
+  it('should create an action to updateFilteredBookmarks with the ids for all bookmarks that have every selected tag in their tags array', () => {
+  });
+
+  it('should create an action to updateFilteredTags with all tags related to the filtered bookmarks that are not selected', () => {
+  });
+
+  // setBookmarks
+  it('should create an action to set the bookmarks object', () => {
+    // SET_BOOKMARKS
+  });
+
+  // setTags
+  it('should create an action to set the tags object', () => {
+    // SET_TAGS
+  });
+
+  // updateFilteredBookmarks
+  it('should create an action to set the filtered bookmarks array', () => {
+    // UPDATE_FILTERED_BOOKMARKS
+  });
+
+  // updateFilteredTags
+  it('should create an action to set the filtered tags array', () => {
+    // UPDATE_FILTERED_TAGS
+  });
+
+  // updateSelectedTags
+  it('should create an action to set the selected tags array', () => {
+    // UPDATE_SELECTED_TAGS
+  });
 })
