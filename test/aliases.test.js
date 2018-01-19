4ab62833012cb73458632e09465747b5c786decd
Cole Murphy <cole.murphy@venuebook.com>
2018-01-18 21:42:55 -0500

    Add testState to alias tests

--- test/aliases.test.js
+++ test/aliases.test.js
@@ -5,7 +5,23 @@ import initialState from '../background/src/store/initialState';
 
 const middlewares = [ thunk ];
 const mockStore = configureMockStore(middlewares);
-const store = mockStore(initialState);
+const testState = {
+  bookmarks: {
+    1: { id: 1, title: 'bookmark', tags: [ 3 ] },
+    2: { id: 2, title: 'other bookmark', tags: [ 3, 4 ] },
+  },
+  filteredBookmarks: [],
+  filteredTags: [],
+  selected: [],
+  sort: {
+    bookmarks: { ascending: true, sortBy: 'date' },
+    tags: { ascending: true, sortBy: 'alpha' },
+  },
+  tags: {
+    3: { id: 3, title: 'tag', bookmarks: [ 1 ] },
+    4: { id: 4, title: 'other tag', bookmarks: [ 1, 2 ] },
+  },
+};
 
 describe('aliases', () => {
   // createBookmark
