8861b9b2ed77362f9537a6a0a7e397795bf20325
Cole Murphy <cole.murphy@venuebook.com>
2018-01-29 23:56:29 -0500

    Fix action tests after updating store

--- test/actions.test.js
+++ test/actions.test.js
@@ -31,16 +31,6 @@ describe('actions', () => {
       const setTagsAction = { type: 'SET_TAGS', data: data.tags };
       expect(storeActions[1]).toEqual(setTagsAction)
     });
-
-    it('should create an action to updateFilteredBookmarks with an empty arrray', () => {
-      const filteredBookmarksAction = { type: 'UPDATE_FILTERED_BOOKMARKS', bookmarks: [] };
-      expect(storeActions[2]).toEqual(filteredBookmarksAction)
-    });
-
-    it('should create an action to updateFilteredTags with all tag ids', () => {
-      const filteredTagsAction = { type: 'UPDATE_FILTERED_TAGS', tags: Object.keys(data.tags) };
-      expect(storeActions[3]).toEqual(filteredTagsAction)
-    });
   })
 
   describe('createOrUpdateBookmark', () => {
@@ -64,50 +54,6 @@ describe('actions', () => {
         tags
       }
       expect(actions.createOrUpdateTags(tags)).toEqual(expectedAction)
-    });
-  })
-
-  describe('filterBookmarksAndTags', () => {
-    const store = mockStore(testState);
-    const storeActions = store.getActions();
-    const storeState = store.getState();
-    const newStore = mockStore(testState);
-    const newStoreActions = newStore.getActions();
-    const selectedTags = [ 3, 4 ];
-    
-    store.dispatch(actions.filterBookmarksAndTags([]))
-
-    it('should create an action to updateFilteredBookmarks with an empty array if no tags are selected', () => {
-      const filterBookmarksAction = { type: 'UPDATE_FILTERED_BOOKMARKS', bookmarks: [] };
-      expect(storeActions[0]).toEqual(filterBookmarksAction)
-    });
-
-    it('should create an action to updateFilteredTags with all tag ids if no tags are selected', () => {
-      const filterTagsAction = { type: 'UPDATE_FILTERED_TAGS', tags: [ "3", "4"] };
-      expect(storeActions[1]).toEqual(filterTagsAction)
-    });
-
-    newStore.dispatch(actions.filterBookmarksAndTags(selectedTags))
-
-    it('should create an action to updateFilteredBookmarks with the ids for all bookmarks that have every selected tag in their tags array', () => {
-      const newFilterBookmarksAction = { type: 'UPDATE_FILTERED_BOOKMARKS', bookmarks: [ 2 ] };
-      const { bookmarks } = newStoreActions[0];
-
-      expect(newStoreActions[0]).toEqual(newFilterBookmarksAction)
-
-      // for each bookmark id
-      bookmarks.forEach(id => {
-        // get the tags associate with that bookmark
-        let { tags } = storeState.bookmarks[id];
-
-        // and make sure every selected tag is in that array
-        expect(selectedTags.every(tagId => tags.includes(tagId))).toEqual(true)
-      })
-    });
-
-    it('should create an action to updateFilteredTags with all tags related to the filtered bookmarks that are not selected', () => {
-      const newFilterTagsAction = { type: 'UPDATE_FILTERED_TAGS', tags: [] };
-      expect(newStoreActions[1]).toEqual(newFilterTagsAction)
     });
   })
 
@@ -136,39 +82,6 @@ describe('actions', () => {
         data
       }
       expect(actions.setTags(data)).toEqual(expectedAction)
-    });
-  })
-
-  describe('updateFilteredBookmarks', () => {
-    it('should create an action to set the filtered bookmarks array', () => {
-      const bookmarks = [1, 2, 3];
-      const expectedAction = {
-        type: 'UPDATE_FILTERED_BOOKMARKS',
-        bookmarks
-      }
-      expect(actions.updateFilteredBookmarks(bookmarks)).toEqual(expectedAction)
-    });
-  })
-
-  describe('updateFilteredTags', () => {
-    it('should create an action to set the filtered tags array', () => {
-      const tags = [1, 2, 3];
-      const expectedAction = {
-        type: 'UPDATE_FILTERED_TAGS',
-        tags
-      }
-      expect(actions.updateFilteredTags(tags)).toEqual(expectedAction)
-    });
-  })
-
-  describe('updateSelectedTags', () => {
-    it('should create an action to set the selected tags array', () => {
-      const tags = [1, 2, 3];
-      const expectedAction = {
-        type: 'UPDATE_SELECTED_TAGS',
-        tags
-      }
-      expect(actions.updateSelectedTags(tags)).toEqual(expectedAction)
     });
   })
 })
