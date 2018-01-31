1d3bb039bc9ad906a704b12cfef7ae55d608f182
Cole Murphy <cole.murphy@venuebook.com>
2018-01-31 16:48:22 -0500

    Update addBookmark function to take a bookmark param

--- background/src/utils/addBookmark.js
+++ background/src/utils/addBookmark.js
@@ -1,5 +1,9 @@
-export default function addBookmark () {
+export default function addBookmark (bookmark) {
   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-    chrome.tabs.sendMessage(tabs[0].id, { ref: 'bookmark_data', data: tabs[0] });
+    let data = tabs[0];
+
+    // if a bookmark object was passed in, send that as the data
+    if (bookmark) data = bookmark;
+    chrome.tabs.sendMessage(tabs[0].id, { ref: 'bookmark_data', data });
   });
 }
