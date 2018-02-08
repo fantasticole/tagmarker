d36ec99aa17b09b38ca332dcd50da5d40f8b35d6
Cole Murphy <cole.murphy@venuebook.com>
2018-02-07 19:48:52 -0500

    Update test state to reflect current state setup

--- test/testState.js
+++ test/testState.js
@@ -3,13 +3,6 @@ export default {
     1: { id: 1, title: 'bookmark', tags: [ 3 ] },
     2: { id: 2, title: 'other bookmark', tags: [ 3, 4 ] },
   },
-  filteredBookmarks: [],
-  filteredTags: [],
-  selected: [ 3 ],
-  sort: {
-    bookmarks: { ascending: true, sortBy: 'date' },
-    tags: { ascending: true, sortBy: 'alpha' },
-  },
   spreadsheetId: null,
   tags: {
     3: { id: 3, title: 'tag', bookmarks: [ 1, 2 ], parents: [ 4 ] },
