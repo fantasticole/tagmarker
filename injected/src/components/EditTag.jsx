50e065cec49cf3c9b8bf6e6656a9d7e4d501cabc
Cole Murphy <cole.murphy@venuebook.com>
2018-02-15 16:24:35 -0500

    update EditTag functions, create editing state

--- injected/src/components/EditTag.jsx
+++ injected/src/components/EditTag.jsx
@@ -20,31 +20,66 @@ export default class EditTag extends Component {
     }
   }
 
-  handleDeleteTag () {
+  handleClickDelete () {
     console.log('delete!')
     // this.props.removeTag(this.props.tag.id)
   }
 
-  handleUpdateTag () {
-    console.log('update!')
+  handleClickCancel () {
+    this.setState({ isEditing: false, title: this.props.tag.title });
+  }
+
+  handleClickSave () {
+    console.log('save!')
     // this.props.updateTag(this.props.tag)
   }
 
-  render () {
-    let { tag } = this.props;
+  handleClickEdit () {
+    this.setState({ isEditing: true });
+  }
 
+  moveFocusToEnd (e) {
+    let { value } = e.target;
+    e.target.value = '';
+    e.target.value = value;
+  }
+
+  render () {
+    let { tag } = this.props,
+        { isEditing, title } = this.state;
+
+    if (isEditing) {
+      return (
+        <li className='tag-item tag-item--is_editing'>
+          <span className='tag'>
+            <input
+              autoFocus
+              className='tag-name__input'
+              defaultValue={title}
+              name='title'
+              onChange={(e) => this.setState({ title: e.target.value })}
+              onFocus={(e) => this.moveFocusToEnd(e)}
+              placeholder='title'
+              type='text'
+              />
+          </span>
+          <span className='edit-tag__actions'>
+            <button className='button edit-tag__button action-button' onClick={() => this.handleClickSave()}>Save <i className='fa fa-floppy-o'/></button>
+            <button className='button edit-tag__button action-button' onClick={() => this.handleClickCancel()}>Cancel <i className='fa fa-ban'/></button>
+            <button className='button edit-tag__button action-button' onClick={() => this.handleClickDelete()}>Delete <i className='fa fa-trash-o'/></button>
+          </span>
+        </li>
+      );
+    }
     return (
       <li className='tag-item tag-item--is_editable'>
-        <button className='button edit-tag__button' onClick={() => this.handleDeleteTag()}>
-          <i className='clear-tag fa fa-times-circle'/>
-        </button>
-        <button className='button edit-tag__button' onClick={() => this.handleUpdateTag()}>
+        <button className='button edit-tag__button' onClick={() => this.handleClickEdit()}>
           <i className='clear-tag fa fa-pencil'/>
         </button>
         <span className='tag-name__wrapper'>
           <MarqueeWrapper>
             <p className='tag'>
-              {tag.title} <span className='tagCount'>{tag.bookmarks.length}</span>
+              {this.state.title} <span className='tagCount'>{tag.bookmarks.length}</span>
             </p>
           </MarqueeWrapper>
         </span>
