2de8b50e56fb6ab8d014d2ed18aad9839620f85d
Cole Murphy <cole.murphy@venuebook.com>
2018-02-16 23:26:44 -0500

    Remove popup builds from gulpfile

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -5,19 +5,8 @@ import rimraf from 'rimraf';
 
 const plugins = loadPlugins();
 
-import popupWebpackConfig from './popup/webpack.config';
 import backgroundWebpackConfig from './background/webpack.config';
 import injectedWebpackConfig from './injected/webpack.config';
-
-gulp.task('popup-js', ['clean'], (cb) => {
-  webpack(popupWebpackConfig, (err, stats) => {
-    if(err) throw new plugins.util.PluginError('webpack', err);
-
-    plugins.util.log('[webpack]', stats.toString());
-
-    cb();
-  });
-});
 
 gulp.task('background-js', ['clean'], (cb) => {
   webpack(backgroundWebpackConfig, (err, stats) => {
@@ -39,12 +28,6 @@ gulp.task('injected-js', ['clean'], (cb) => {
   });
 });
 
-gulp.task('popup-html', ['clean'], () => {
-  return gulp.src('popup/src/index.html')
-    .pipe(plugins.rename('popup.html'))
-    .pipe(gulp.dest('./build'))
-});
-
 gulp.task('copy-manifest', ['clean'], () => {
   return gulp.src('manifest.json')
     .pipe(gulp.dest('./build'));
@@ -64,10 +47,9 @@ gulp.task('clean', (cb) => {
   rimraf('./build', cb);
 });
 
-gulp.task('build', ['copy-assets', 'copy-content', 'copy-manifest', 'popup-js', 'popup-html', 'background-js', 'injected-js']);
+gulp.task('build', ['copy-assets', 'copy-content', 'copy-manifest', 'background-js', 'injected-js']);
 
 gulp.task('watch', ['default'], () => {
-  gulp.watch('popup/**/*', ['build']);
   gulp.watch('injected/**/*', ['build']);
   gulp.watch('background/**/*', ['build']);
 });
