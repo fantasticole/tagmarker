8adfc44c8f249d5ce723f0fab69e39ba97f9d745
Cole Murphy <cole.murphy@venuebook.com>
2018-02-20 10:32:51 -0500

    Begin rendering multiple bookmarks in ManageBookmarkModal

--- injected/src/components/Drawer.jsx
+++ injected/src/components/Drawer.jsx
@@ -11,25 +11,27 @@ import Modal from './Modal';
  *
  * @param {function} addTag - function to add a tag
  * @param {function} createBookmark - function to save a bookmark
- * @param {function} updateBookmark - function to update a bookmark
  * @param {object} tags - all tags from store
+ * @param {function} updateBookmark - function to update a bookmark
  */
 export default class Drawer extends Component {
   constructor (props) {
     super(props);
+
+    this.state = {
+      bookmarks: [],
+    }
   }
 
   componentDidMount () {
     // listen for flags to add bookmark
     chrome.runtime.onMessage.addListener((req, sender, sendResponse) => {
-      // if we're being sent bookmark data, open modal to add bookmark
+      // if we're being sent bookmark data, add it to the state
       if (req.ref === 'bookmark_data') {
         // I don't understand why req.data has tags for manually
         // created bookmarks
-        let { data, drawerWasOpen, suggestedTags, update } = req,
-            { manageTagAndBookmark, createBookmark, tags, updateBookmark } = this.props,
-            manageBookmark = update ? updateBookmark : createBookmark,
-            closeDrawer = drawerWasOpen === undefined ? drawerWasOpen : (!drawerWasOpen),
+        let { bookmarks, closeDrawer } = this.state,
+            { data, drawerWasOpen, suggestedTags, update } = req,
             bookmarkData = {
               bookmark: data,
               selected: data.tags,
@@ -37,22 +39,37 @@ export default class Drawer extends Component {
               update,
             };
 
-        Modal.render(
-          <ManageBookmarkModal
-            manageTagAndBookmark={(...args) => manageTagAndBookmark(...args)}
-            manageBookmark={(...args) => manageBookmark(...args)}
-            data={bookmarkData}
-            onCloseModal={() => this.handleModalClose(closeDrawer)}
-            tags={tags}
-            />
-        );
+        // if closeDrawer hasn't been set yet, set it
+        if (!this.state.hasOwnProperty('closeDrawer')) {
+          closeDrawer = drawerWasOpen === undefined ? drawerWasOpen : (!drawerWasOpen);
+        }
+
+        bookmarks.push(bookmarkData);
+        this.setState({ bookmarks, closeDrawer })
       }
     })
   }
 
-  handleModalClose (closeDrawer) {
+  componentDidUpdate (prevProps, prevState) {
+    if (this.state.bookmarks.length) {
+      let { manageTagAndBookmark, createBookmark, tags, updateBookmark } = this.props;
+
+      Modal.render(
+        <ManageBookmarkModal
+          createBookmark={(bookmark, tagsToAdd) => createBookmark(bookmark, tagsToAdd)}
+          data={this.state.bookmarks}
+          manageTagAndBookmark={(...args) => manageTagAndBookmark(...args)}
+          onCloseModal={() => this.handleModalClose()}
+          tags={tags}
+          updateBookmark={(bookmark) => updateBookmark(bookmark)}
+          />
+      );
+    }
+  }
+
+  handleModalClose () {
     // if we should close the drawer, send message to do so
-    if (closeDrawer) chrome.runtime.sendMessage({ ref: 'toggle_drawer' });
+    if (this.state.closeDrawer) chrome.runtime.sendMessage({ ref: 'toggle_drawer' });
   }
 
   render () {
--- injected/src/components/ManageBookmarkModal.jsx
+++ injected/src/components/ManageBookmarkModal.jsx
@@ -7,12 +7,13 @@ import Modal from './Modal';
 /**
  * ManageBookmarkModal
  *
+ * @param {function} createBookmark - function to save a bookmark
  * @param {object} data - data for bookmark object we're managing
  * @param {function} manageTagAndBookmark - function to add tag and manage
  * a bookmark
- * @param {function} manageBookmark - function to manage a bookmark
  * @param {function} onCloseModal - function to run when modal closes
  * @param {object} tags - all tags from store
+ * @param {function} updateBookmark - function to update a bookmark
  */
 export default class ManageBookmarkModal extends Component {
   constructor (props) {
@@ -25,21 +26,29 @@ export default class ManageBookmarkModal extends Component {
   }
 
   render () {
-    let { data, tags } = this.props,
-        { bookmark, selected, suggested, update } = data;
+    let { createBookmark, data, tags, updateBookmark } = this.props;
 
     return (
       <Modal.Modal className='bookmark-modal' onClose={() => this.props.onCloseModal()} ref='modal'>
-        <EditBookmarkView
-          bookmark={bookmark}
-          manageTagAndBookmark={(...args) => manageTagAndBookmark(...args)}
-          manageBookmark={(...args) => manageBookmark(...args)}
-          onDeactivate={() => this.handleDeactivate(bookmark.id)}
-          selected={selected}
-          suggested={suggested}
-          tags={tags}
-          update={update}
-          />
+        {data.map(bookmarkData => {
+          let { bookmark, selected, suggested, update } = bookmarkData,
+              manageBookmark = update ? updateBookmark : createBookmark;
+
+          return (
+            <EditBookmarkView
+              bookmark={bookmark}
+              key={bookmark.id}
+              manageTagAndBookmark={(...args) => manageTagAndBookmark(...args)}
+              manageBookmark={(...args) => manageBookmark(...args)}
+              onDeactivate={() => this.handleDeactivate(bookmark.id)}
+              selected={selected}
+              suggested={suggested}
+              tags={tags}
+              update={update}
+              />
+          );
+        })
+      }
       </Modal.Modal>
     );
   }
